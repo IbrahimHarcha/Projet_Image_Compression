a2998f2aa4563766b58ee9f9b5761c1fe88695e1
Galax416 <emmanuelfontaine416@gmail.com>
2024-03-07 20:56:29 +0100

    Create main.py

---
+++ Code/src/main.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+def main():
+    # Créer la fenêtre principale
+    root = tk.Tk()
+    root.title("Détection de Falsification d'Images")
+
+    # Ajoute d'autres éléments à la fenêtre
+
+
+    # Lancer la boucle principale
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
+
